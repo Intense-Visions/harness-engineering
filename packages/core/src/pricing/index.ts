47d67f8c536c13f895eb570533fb766f6d8f60c1
Chad Warner <chad.warner@capillarytech.com>
2026-03-31 10:33:35 -0400

    feat(pricing): wire pricing module exports through core index

---
+++ packages/core/src/pricing/index.ts
@@ -0,0 +1,15 @@
+export { getModelPrice, parseLiteLLMData } from './pricing';
+export {
+  loadPricingData,
+  LITELLM_PRICING_URL,
+  CACHE_TTL_MS,
+  STALENESS_WARNING_DAYS,
+} from './cache';
+export { calculateCost } from './calculator';
+export type {
+  PricingDataset,
+  PricingCacheFile,
+  LiteLLMModelEntry,
+  LiteLLMPricingData,
+  FallbackPricingFile,
+} from './types';
