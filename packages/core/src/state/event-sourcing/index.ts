ccbb523941d174d4e7d0be5eebb04bbf2b6fdfdb
Chad Warner <chad.warner@gmail.com>
2026-06-26 21:02:05 -0400

    feat(core/event-sourcing): export public log-core API from module barrel

--- packages/core/src/state/event-sourcing/index.ts
+++ packages/core/src/state/event-sourcing/index.ts
@@ -1,2 +1,7 @@
 // packages/core/src/state/event-sourcing/index.ts
 export * from './constants';
+export { EventSchema, StoredEventSchema, BlobRefSchema, ScopeSchema, isBlobRef } from './events';
+export type { Event, EventType, EventInput, StoredEvent, BlobRef, Scope } from './events';
+export { getWriterId } from './writer-id';
+export { emitEvent, loadEvents, readTailSeq, eventLogPaths } from './log';
+export type { EventLogOptions, EventLogPaths, EmitResult } from './log';
